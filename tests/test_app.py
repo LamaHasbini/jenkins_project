ea043da8acab6e5995ee1b674d5b7e79537be6db
lamahasbini <lhh27@mail.aub.edu>
2024-11-13 19:58:44 +0200

    Lint fixing

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,8 @@ from app import greet
 class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
-        
+
 if __name__ == "__main__":
     unittest.main()
+
+    
